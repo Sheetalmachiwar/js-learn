6e0aaafc8b80be31cd792be04eb4c2dde36a4d11
Sheetal Machiwar <109799140+Sheetalmachiwar@users.noreply.github.com>
2025-01-16 09:18:25 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+
+//"use strict" treat all js code as newer version
+//alerrt(3+3)we are using node.js ,not browser
+/*
+datatypes
+number
+bigint
+string
+boolean
+null=standalone value == type of null is a object
+undefined
+symbol = unique
+ 
+
+object
+
+*/
